7be29d8ab82a9cbba5b97e441ef2c3c90b692d73
Nitin <rajnitin793@gmail.com>
2026-07-11 15:07:38 +0530

    created estimate_altitude_km functoin in propagator module via Kepler's third law for cheap pre-filtering

--- backend/app/orbit/propagator.py
+++ backend/app/orbit/propagator.py
@@ -1,5 +1,5 @@
 from skyfield.api import load, EarthSatellite, wgs84
-
+import math
 from app.models.satellite import Satellite
 
 _ts = load.timescale()
@@ -7,7 +7,7 @@ _ts = load.timescale()
 
 def get_current_position(satellite: Satellite):
     """
-    Takes our own Satellite model (with line1, line2, name)
+    taking our own Satellite model (with line1, line2, name)
     and returns its current latitude, longitude, and altitude.
     """
     sky_satellite = EarthSatellite(satellite.line1, satellite.line2, satellite.name)
@@ -49,4 +49,20 @@ def get_xyz_at_time(satellite: Satellite, time):
     return {
         "position": (float(x), float(y), float(z)),
     }
+
+def estimate_altitude_km(satellite: Satellite) -> float:
+    """
+    estimating orbital altitude from mean_motion using Kepler's third law.
+    Much cheaper than full SGP4 propagation — used for pre-filtering
+    satellite pairs before expensive collision checks.
+    """
+    MU = 398600.4418  
+    EARTH_RADIUS_KM = 6378.137
+
+     
+    n = satellite.mean_motion * 2*math.pi/86400
+   
+    a = (MU / n**2) ** (1/3)
     
+   
+    return a-EARTH_RADIUS_KM 
--- backend/sc.py
+++ backend/sc.py
@@ -1,5 +1,15 @@
-from app.services.satellite_query_service import get_all_close_approaches
+from app.orbit.propagator import estimate_altitude_km
+from app.models.satellite import Satellite
 
-results = get_all_close_approaches()
-print("Count:", len(results))
-print(results[0].satellite_1_norad_id, results[0].min_distance_km)
+my_sat = Satellite(
+    name="CALSPHERE 2",
+    line1="1 00902U 64063E   26189.99802150  .00000023  00000+0  20893-4 0  9990",
+    line2="2 00902  90.2354  76.3174 0019161 158.2203 267.4625 13.52902150859218",
+    norad_id="00902",
+    inclination=90.2354,
+    eccentricity=0.0019161,
+    mean_motion=13.52902150859218,
+    orbital_period_minutes=105.7
+)
+
+print(estimate_altitude_km(my_sat))   
